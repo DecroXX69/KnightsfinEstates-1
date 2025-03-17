09790887d1130536017d29eb26bbffc5a309fefe
Atreusx1 <anishkadam92@gmail.com>
2025-03-17 12:36:36 +0530

    under construction

--- dev-app/src/components/PropertyListingPage.jsx
+++ dev-app/src/components/PropertyListingPage.jsx
@@ -370,7 +370,7 @@ const PropertyListingPage = () => {
                     {property.subStatus === 'available' && (
                       <div className={styles.availableBadge}>Ready to Move</div>
                     )}
-                    {property.subStatus === 'under construction' && (
+                    {property.subStatus === 'Under Construction' && (
                       <div className={styles.constructionBadge}>Under Construction</div>
                     )}
                   </div>
